3d75d36a0d8f3b89d000c35f8e43bfe9d9e810dc
kotavenkatakrishnavamsi <kotavenkatakrishnavamsi@gmail.com>
2024-01-22 19:52:26 +0530

    added file

---
+++ login.java
@@ -0,0 +1,15 @@
+public class LoginUser
+{
+        public static void main(String args[])
+        {
+                int i;
+
+                for(i=0;i<=60;i++){
+
+                
+
+                        System.out.println("Number: " + i);
+                }
+        }
+}
+
